fc97dd5972baff5607a6d30d9a98dd09a8e12f21
Maverick Tesini <mavericktesini@gmail.com>
2018-03-20 14:51:43 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize
+    @word  = word
+  end
+
+  def match(word_array)
+    # @word_array = word array
+    word_array.split(" ")
+    binding.pry
+  end
